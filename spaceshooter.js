82d0c253a994089f49fcb62a59a11276eb3e55df
福島匠 <fukushimatakumi@fukushimatakuminoMacBook-Pro.local>
2023-05-19 16:06:32 +0900

    何度目の変更か

--- spaceshooter.js
+++ spaceshooter.js
@@ -1,199 +1,252 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Game canvas
-    const canvas = document.getElementById("gameCanvas");
-    const ctx = canvas.getContext("2d");
-  
-    // Player
-    const playerWidth = 50;
-    const playerHeight = 50;
-    const playerSpeed = 5;
-    const player = {
-      x: canvas.width / 2 - playerWidth / 2,
-      y: canvas.height - playerHeight - 10,
-      width: playerWidth,
-      height: playerHeight,
-      isMovingLeft: false,
-      isMovingRight: false
-    };
-  
-    // Laser
-    const laserWidth = 10;
-    const laserHeight = 20;
-    const laserSpeed = 10;
-    const lasers = [];
-  
-    // Enemy
-    const enemyWidth = 50;
-    const enemyHeight = 50;
-    const enemySpeed = 2;
-    const enemies = [];
-  
-    // Game state
-    let score = 0;
-    let isGameOver = false;
-  
-    // Game over text
-    const gameOverText = document.getElementById("gameOverText");
-  
-    // Retry button
-    const retryButton = document.getElementById("retryButton");
-  
-    // Event listeners
-    document.addEventListener("keydown", handleKeyDown);
-    document.addEventListener("keyup", handleKeyUp);
-    retryButton.addEventListener("click", startGame);
-  
-    // Start the game
-    startGame();
-  
-    function startGame() {
-      isGameOver = false;
-      score = 0;
-      player.x = canvas.width / 2 - playerWidth / 2;
-      player.isMovingLeft = false;
-      player.isMovingRight = false;
-      lasers.length = 0;
-      enemies.length = 0;
-      spawnEnemy();
-      draw();
-      update();
-      gameOverText.style.display = "none";
-      retryButton.style.display = "none";
+// ゲームの状態を表す変数
+let isGameOver;
+let score;
+
+// プレーヤーの情報を表すオブジェクト
+let player;
+const playerWidth = 50;
+const playerHeight = 50;
+
+// ミサイルの情報を表すオブジェクト
+let missile;
+const missileWidth = 10;
+const missileHeight = 30;
+const missileSpeed = 5;
+
+// 敵機の情報を表すオブジェクト
+let enemy;
+const enemyWidth = 50;
+const enemyHeight = 50;
+const enemySpeed = 2;
+
+// キー入力の状態を表すオブジェクト
+let keys = {};
+
+// 描画用のキャンバスとコンテキスト
+let canvas;
+let ctx;
+
+// スタートボタンとリトライボタン
+let startButton;
+let retryButton;
+
+// ゲームの初期化処理
+function init() {
+  isGameOver = true;
+  score = 0;
+
+  player = {
+    x: canvas.width / 2 - playerWidth / 2,
+    y: canvas.height - playerHeight - 10,
+    width: playerWidth,
+    height: playerHeight,
+  };
+
+  missile = {
+    x: 0,
+    y: 0,
+    width: missileWidth,
+    height: missileHeight,
+    isActive: false,
+  };
+
+  enemy = {
+    x: 0,
+    y: 0,
+    width: enemyWidth,
+    height: enemyHeight,
+    isActive: false,
+  };
+
+  canvas.addEventListener('keydown', handleKeyDown);
+  canvas.addEventListener('keyup', handleKeyUp);
+
+  startButton = createButton('Start', startGame);
+  retryButton = createButton('Retry', restartGame);
+
+  updateScore();
+  updateButtons();
+}
+
+// ゲームの開始処理
+function startGame() {
+  isGameOver = false;
+  hideButton(startButton);
+  hideButton(retryButton);
+  requestAnimationFrame(gameLoop);
+}
+
+// ゲームのリスタート処理
+function restartGame() {
+  showButton(startButton);
+  hideButton(retryButton);
+  init();
+}
+
+// ゲームループ
+function gameLoop() {
+  if (isGameOver) {
+    return;
+  }
+
+  update();
+  render();
+
+  requestAnimationFrame(gameLoop);
+}
+
+// ゲームの状態を更新
+function update() {
+  movePlayer();
+  moveMissile();
+  moveEnemy();
+
+  checkCollision();
+}
+
+// ゲーム画面を描画
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawPlayer();
+  drawMissile();
+  drawEnemy();
+}
+
+// プレーヤーの移動処理
+function movePlayer() {
+  if (keys.ArrowLeft && player.x > 0) {
+    player.x -= 5;
+  }
+
+  if (keys.ArrowRight && player.x < canvas.width - player.width) {
+    player.x += 5;
+  }
+}
+
+// ミサイルの移動処理
+function moveMissile() {
+  if (missile.isActive) {
+    missile.y -= missileSpeed;
+
+    if (missile.y < 0) {
+      missile.isActive = false;
     }
-  
-    function handleKeyDown(event) {
-      if (event.key === "ArrowLeft") {
-        player.isMovingLeft = true;
-      } else if (event.key === "ArrowRight") {
-        player.isMovingRight = true;
-      }
-    }
-  
-    function handleKeyUp(event) {
-      if (event.key === "ArrowLeft") {
-        player.isMovingLeft = false;
-      } else if (event.key === "ArrowRight") {
-        player.isMovingRight = false;
-      }
-    }
-  
-    function update() {
-      if (isGameOver) {
-        return;
-      }
-  
-      // Move player
-      if (player.isMovingLeft && player.x > 0) {
-        player.x -= playerSpeed;
-      } else if (player.isMovingRight && player.x + player.width < canvas.width) {
-        player.x += playerSpeed;
-      }
-  
-      // Move lasers
-      for (let i = 0; i < lasers.length; i++) {
-        const laser = lasers[i];
-        laser.y -= laserSpeed;
-  
-        // Remove lasers that are out of bounds
-        if (laser.y < 0) {
-          lasers.splice(i, 1);
-          i--;
-        }
-      }
-  
-      // Move enemies and check collision with player
-      for (let i = 0; i < enemies.length; i++) {
-        const enemy = enemies[i];
-        enemy.y += enemySpeed;
-  
-        // Check collision with player
-        if (checkCollision(player, enemy)) {
-          gameOver();
-          return;
-        }
-  
-        // Remove enemies that are out of bounds
-        if (enemy.y > canvas.height) {
-          enemies.splice(i, 1);
-          i--;
-        }
-      }
-  
-      // Check collision between lasers and enemies
-      for (let i = 0; i < lasers.length; i++) {
-        const laser = lasers[i];
-  
-        for (let j = 0; j < enemies.length; j++) {
-          const enemy = enemies[j];
-  
-          if (checkCollision(laser, enemy)) {
-            lasers.splice(i, 1);
-            i--;
-            enemies.splice(j, 1);
-            j--;
-            score++;
-            break;
-          }
-        }
-      }
-  
-      spawnEnemy();
-      draw();
-      requestAnimationFrame(update);
-    }
-  
-    function draw() {
-      // Clear canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-      // Draw player
-      ctx.fillStyle = "red";
-      ctx.fillRect(player.x, player.y, player.width, player.height);
-  
-      // Draw lasers
-      ctx.fillStyle = "green";
-      for (const laser of lasers) {
-        ctx.fillRect(laser.x, laser.y, laser.width, laser.height);
-      }
-  
-      // Draw enemies
-      ctx.fillStyle = "blue";
-      for (const enemy of enemies) {
-        ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
-      }
-  
-      // Draw score
-      ctx.fillStyle = "black";
-      ctx.font = "24px Arial";
-      ctx.fillText("Score: " + score, 10, 30);
-    }
-  
-    function spawnEnemy() {
-      if (Math.random() < 0.02) {
-        const enemyX = Math.random() * (canvas.width - enemyWidth);
-        const enemy = {
-          x: enemyX,
-          y: 0,
-          width: enemyWidth,
-          height: enemyHeight
-        };
-        enemies.push(enemy);
-      }
-    }
-  
-    function gameOver() {
-      isGameOver = true;
-      gameOverText.style.display = "block";
-      retryButton.style.display = "block";
-    }
-  
-    function checkCollision(rect1, rect2) {
-      return (
-        rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.y + rect1.height > rect2.y
-      );
-    }
-  });
-  
+  }
+}
+
+// 敵機の移動処理
+function moveEnemy() {
+  if (!enemy.isActive) {
+    enemy.x = Math.random() * (canvas.width - enemy.width);
+    enemy.y = -enemy.height;
+    enemy.isActive = true;
+  }
+
+  enemy.y += enemySpeed;
+
+  if (enemy.y > canvas.height) {
+    enemy.isActive = false;
+  }
+}
+
+// 衝突判定の処理
+function checkCollision() {
+  if (isColliding(player, enemy) || isColliding(missile, enemy)) {
+    gameOver();
+  }
+}
+
+// ゲームオーバー処理
+function gameOver() {
+  isGameOver = true;
+  showButton(retryButton);
+}
+
+// 衝突判定
+function isColliding(obj1, obj2) {
+  return (
+    obj1.x < obj2.x + obj2.width &&
+    obj1.x + obj1.width > obj2.x &&
+    obj1.y < obj2.y + obj2.height &&
+    obj1.y + obj1.height > obj2.y
+  );
+}
+
+// プレーヤーを描画
+function drawPlayer() {
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+// ミサイルを描画
+function drawMissile() {
+  if (missile.isActive) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(missile.x, missile.y, missile.width, missile.height);
+  }
+}
+
+// 敵機を描画
+function drawEnemy() {
+  if (enemy.isActive) {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+  }
+}
+
+// スコアを更新
+function updateScore() {
+  document.getElementById('score').innerText = `Score: ${score}`;
+}
+
+// キーの押下状態を記録
+function handleKeyDown(e) {
+  keys[e.code] = true;
+
+  if (e.code === 'Space' && !missile.isActive) {
+    fireMissile();
+  }
+}
+
+// キーの解放状態を記録
+function handleKeyUp(e) {
+  keys[e.code] = false;
+}
+
+// ミサイルを発射
+function fireMissile() {
+  missile.x = player.x + player.width / 2 - missile.width / 2;
+  missile.y = player.y;
+  missile.isActive = true;
+}
+
+// ボタンを作成
+function createButton(text, clickHandler) {
+  const button = document.createElement('button');
+  button.innerText = text;
+  button.addEventListener('click', clickHandler);
+  document.body.appendChild(button);
+  return button;
+}
+
+// ボタンを表示
+function showButton(button) {
+  button.style.display = 'block';
+}
+
+// ボタンを非表示
+function hideButton(button) {
+  button.style.display = 'none';
+}
+
+// 初期化とゲームの開始
+function start() {
+  canvas = document.getElementById('gameCanvas');
+  ctx = canvas.getContext('2d');
+
+  init();
+}
+
+// ページの読み込み後にゲームを開始
+window.addEventListener('load', start);
