6bc24d03f680f4618981916a6e4c490dbabf82e4
福島匠 <fukushimatakumi@fukushimatakuminoMacBook-Pro.local>
2023-05-19 14:43:57 +0900

    敵機移動＋自動レーザー

--- spaceshooter.js
+++ spaceshooter.js
@@ -2,94 +2,130 @@ let canvas = document.getElementById('gameCanvas');
 let context = canvas.getContext('2d');
 
 let player = {
-  x: 400,
-  y: 300,
-  width: 50,
-  height: 50,
-  dx: 4,  // Player's speed has been doubled
-  dy: 4   // Player's speed has been doubled
+    x: 400,
+    y: 500,
+    width: 50,
+    height: 50,
+    dx: 4
 };
 
-let lasers = [];
-
-let enemies = [
-  {x: 100, y: 100, width: 50, height: 50},
-  {x: 200, y: 200, width: 50, height: 50},
-  {x: 300, y: 300, width: 50, height: 50}
-]; // Multiple enemies have been added
-
+let playerLasers = [];
+let enemyLasers = [];
+let enemies = [];
 let keys = {};
 let score = 0;
-document.getElementById('score').innerHTML = "Score: " + score;
+let gameInterval;
+
+document.getElementById('startButton').addEventListener('click', startGame);
 
 window.addEventListener('keydown', function(e) {
-  keys[e.keyCode] = true;
-  if (e.keyCode === 32) {
-    lasers.push({
-      x: player.x + player.width / 2,
-      y: player.y,
-      width: 5,
-      height: 15,
-      dy: -4
-    });
-  }
+    keys[e.keyCode] = true;
 }, false);
 
 window.addEventListener('keyup', function(e) {
-  delete keys[e.keyCode];
+    delete keys[e.keyCode];
 }, false);
 
+function startGame() {
+    gameInterval = setInterval(game, 10);
+    setInterval(spawnEnemy, 1000);
+}
+
 function game() {
-  update();
-  render();
-  collisionDetection();
-  window.requestAnimationFrame(game);
+    update();
+    render();
+    collisionDetection();
 }
 
 function update() {
-  if (keys[87]) player.y -= player.dy; // w
-  if (keys[83]) player.y += player.dy; // s
-  if (keys[65]) player.x -= player.dx; // a
-  if (keys[68]) player.x += player.dx; // d
+    if (keys[37]) player.x -= player.dx; // left arrow key
+    if (keys[39]) player.x += player.dx; // right arrow key
 
-  for (let i = 0; i < lasers.length; i++) {
-    lasers[i].y += lasers[i].dy;
-  }
+    playerLasers.push({
+        x: player.x + player.width / 2,
+        y: player.y,
+        width: 5,
+        height: 15,
+        dy: -4
+    });
+
+    for (let i = 0; i < playerLasers.length; i++) {
+        playerLasers[i].y += playerLasers[i].dy;
+    }
+
+    for (let i = 0; i < enemies.length; i++) {
+        if (Math.random() < 0.002) {
+            enemyLasers.push({
+                x: enemies[i].x,
+                y: enemies[i].y,
+                width: 5,
+                height: 15,
+                dy: 2
+            });
+        }
+    }
+
+    for (let i = 0; i < enemyLasers.length; i++) {
+        enemyLasers[i].y += enemyLasers[i].dy;
+    }
 }
 
 function render() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  
-  context.fillStyle = 'green';
-  context.fillRect(player.x, player.y, player.width, player.height);
-  
-  context.fillStyle = 'red';
-  for (let i = 0; i < enemies.length; i++) {
-    context.fillRect(enemies[i].x, enemies[i].y, enemies[i].width, enemies[i].height);
-  }
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-  context.fillStyle = 'orange';
-  for (let i = 0; i < lasers.length; i++) {
-    context.fillRect(lasers[i].x, lasers[i].y, lasers[i].width, lasers[i].height);
-  }
+    context.fillStyle = 'green';
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+    context.fillStyle = 'red';
+    for (let i = 0; i < enemies.length; i++) {
+        context.fillRect(enemies[i].x, enemies[i].y, enemies[i].width, enemies[i].height);
+    }
+
+    context.fillStyle = 'orange';
+    for (let i = 0; i < playerLasers.length; i++) {
+        context.fillRect(playerLasers[i].x, playerLasers[i].y, playerLasers[i].width, playerLasers[i].height);
+    }
+
+    context.fillStyle = 'blue';
+    for (let i = 0; i < enemyLasers.length; i++) {
+        context.fillRect(enemyLasers[i].x, enemyLasers[i].y, enemyLasers[i].width, enemyLasers[i].height);
+    }
 }
 
 function collisionDetection() {
-  for (let i = 0; i < lasers.length; i++) {
-    for (let j = 0; j < enemies.length; j++) {
-      if (lasers[i].x < enemies[j].x + enemies[j].width &&
-        lasers[i].x + lasers[i].width > enemies[j].x &&
-        lasers[i].y < enemies[j].y + enemies[j].height &&
-        lasers[i].y + lasers[i].height > enemies[j].y) {
-        // collision detected!
-        enemies.splice(j, 1);  // remove enemy
-        lasers.splice(i, 1);   // remove laser
-        score++;
-        document.getElementById('score').innerHTML = "Score: " + score;
-        break;
-      }
+    for (let i = 0; i < playerLasers.length; i++) {
+        for (let j = 0; j < enemies.length; j++) {
+            if (playerLasers[i].x < enemies[j].x + enemies[j].width &&
+                playerLasers[i].x + playerLasers[i].width > enemies[j].x &&
+                playerLasers[i].y < enemies[j].y + enemies[j].height &&
+                playerLasers[i].y + playerLasers[i].height > enemies[j].y) {
+                enemies.splice(j, 1);
+                playerLasers.splice(i, 1);
+                score++;
+                document.getElementById('score').innerHTML = "Score: " + score;
+                break;
+            }
+        }
     }
-  }
+
+    for (let i = 0; i < enemyLasers.length; i++) {
+        if (enemyLasers[i].x < player.x + player.width &&
+            enemyLasers[i].x + enemyLasers[i].width > player.x &&
+            enemyLasers[i].y < player.y + player.height &&
+            enemyLasers[i].y + enemyLasers[i].height > player.y) {
+            clearInterval(gameInterval);
+            alert('Game Over!');
+            document.location.reload();
+        }
+    }
 }
 
-game();
+function spawnEnemy() {
+    enemies.push({
+        x: Math.random() * (canvas.width - 50),
+        y: 0,
+        width: 50,
+        height: 50
+    });
+}
+
