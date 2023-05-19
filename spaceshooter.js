5c7b6bac8ae37dd42892ef4fc8703726cd8f6c52
福島匠 <fukushimatakumi@fukushimatakuminoMacBook-Pro.local>
2023-05-19 15:12:04 +0900

    衝突判定とモバイル対応

--- spaceshooter.js
+++ spaceshooter.js
@@ -1,131 +1,177 @@
-let canvas = document.getElementById('gameCanvas');
-let context = canvas.getContext('2d');
+var canvas = document.getElementById('gameCanvas');
+var context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let player = {
-    x: 400,
-    y: 500,
+var score = 0;
+
+var player = {
+    x: canvas.width / 2,
+    y: canvas.height - 50,
     width: 50,
     height: 50,
-    dx: 4
+    dx: 0
 };
 
-let playerLasers = [];
-let enemyLasers = [];
-let enemies = [];
-let keys = {};
-let score = 0;
-let gameInterval;
+var enemies = [];
+var playerLasers = [];
+var enemyLasers = [];
 
-document.getElementById('startButton').addEventListener('click', startGame);
+var lastEnemySpawnTime = 0;
+var lastPlayerLaserTime = 0;
+var lastEnemyLaserTime = 0;
 
-window.addEventListener('keydown', function(e) {
-    keys[e.keyCode] = true;
-}, false);
+// Controls
+document.getElementById('leftBtn').addEventListener('touchstart', function() {
+    player.dx = -2;
+});
 
-window.addEventListener('keyup', function(e) {
-    delete keys[e.keyCode];
-}, false);
+document.getElementById('rightBtn').addEventListener('touchstart', function() {
+    player.dx = 2;
+});
 
-function startGame() {
-    gameInterval = setInterval(game, 10);
-    setInterval(spawnEnemy, 1000);
-}
+document.getElementById('leftBtn').addEventListener('touchend', function() {
+    player.dx = 0;
+});
 
-function game() {
-    update();
-    render();
-    collisionDetection();
-}
+document.getElementById('rightBtn').addEventListener('touchend', function() {
+    player.dx = 0;
+});
 
-function update() {
-    if (keys[37]) player.x -= player.dx; // left arrow key
-    if (keys[39]) player.x += player.dx; // right arrow key
+var gameInterval = setInterval(gameLoop, 10);
 
-    playerLasers.push({
-        x: player.x + player.width / 2,
-        y: player.y,
-        width: 5,
-        height: 15,
-        dy: -4
-    });
+function gameLoop() {
+    // Clear screen
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < playerLasers.length; i++) {
-        playerLasers[i].y += playerLasers[i].dy;
+    // Draw player
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+    // Move player
+    player.x += player.dx;
+
+    // Enemy spawn
+    if (Date.now() - lastEnemySpawnTime > 1000) {
+        var enemy = {
+            x: Math.random() * (canvas.width - 50),
+            y: 0,
+            width: 50,
+            height: 50,
+            dy: 1
+        };
+        enemies.push(enemy);
+        lastEnemySpawnTime = Date.now();
     }
 
-    for (let i = 0; i < enemies.length; i++) {
-        if (Math.random() < 0.002) {
-            enemyLasers.push({
-                x: enemies[i].x,
-                y: enemies[i].y,
+    // Draw enemies and handle enemy lasers
+    for (var i = 0; i < enemies.length; i++) {
+        var enemy = enemies[i];
+        context.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+        enemy.y += enemy.dy;
+
+        if (Date.now() - lastEnemyLaserTime > 500) {
+            var laser = {
+                x: enemy.x + 25,
+                y: enemy.y + 50,
                 width: 5,
-                height: 15,
+                height: 10,
                 dy: 2
-            });
+            };
+            enemyLasers.push(laser);
+            lastEnemyLaserTime = Date.now();
         }
     }
 
-    for (let i = 0; i < enemyLasers.length; i++) {
-        enemyLasers[i].y += enemyLasers[i].dy;
-    }
-}
-
-function render() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    context.fillStyle = 'green';
-    context.fillRect(player.x, player.y, player.width, player.height);
-
-    context.fillStyle = 'red';
-    for (let i = 0; i < enemies.length; i++) {
-        context.fillRect(enemies[i].x, enemies[i].y, enemies[i].width, enemies[i].height);
+    // Player laser spawn
+    if (Date.now() - lastPlayerLaserTime > 500) {
+        var laser = {
+            x: player.x + 25,
+            y: player.y,
+            width: 5,
+            height: 10,
+            dy: -2
+        };
+        playerLasers.push(laser);
+        lastPlayerLaserTime = Date.now();
     }
 
-    context.fillStyle = 'orange';
-    for (let i = 0; i < playerLasers.length; i++) {
-        context.fillRect(playerLasers[i].x, playerLasers[i].y, playerLasers[i].width, playerLasers[i].height);
+    // Draw lasers
+    for (var i = 0; i < playerLasers.length; i++) {
+        var laser = playerLasers[i];
+        context.fillRect(laser.x, laser.y, laser.width, laser.height);
+        laser.y += laser.dy;
+
+        if (laser.y + laser.height < 0) {
+            playerLasers.splice(i, 1);
+            i--;
+        }
     }
 
-    context.fillStyle = 'blue';
-    for (let i = 0; i < enemyLasers.length; i++) {
-        context.fillRect(enemyLasers[i].x, enemyLasers[i].y, enemyLasers[i].width, enemyLasers[i].height);
-    }
-}
+    // Draw enemy lasers
+    for (var i = 0; i < enemyLasers.length; i++) {
+        var laser = enemyLasers[i];
+        context.fillRect(laser.x, laser.y, laser.width, laser.height);
+        laser.y += laser.dy;
 
-function collisionDetection() {
-    for (let i = 0; i < playerLasers.length; i++) {
-        for (let j = 0; j < enemies.length; j++) {
-            if (playerLasers[i].x < enemies[j].x + enemies[j].width &&
-                playerLasers[i].x + playerLasers[i].width > enemies[j].x &&
-                playerLasers[i].y < enemies[j].y + enemies[j].height &&
-                playerLasers[i].y + playerLasers[i].height > enemies[j].y) {
-                enemies.splice(j, 1);
+        if (laser.y > canvas.height) {
+            enemyLasers.splice(i, 1);
+            i--;
+        }
+    }
+
+    // Collision detection
+    for (var i = playerLasers.length - 1; i >= 0; i--) {
+        var laser = playerLasers[i];
+        for (var j = enemies.length - 1; j >= 0; j--) {
+            var enemy = enemies[j];
+            if (laser.x < enemy.x + enemy.width &&
+                laser.x + laser.width > enemy.x &&
+                laser.y < enemy.y + enemy.height &&
+                laser.y + laser.height > enemy.y) {
+                // Collision detected
                 playerLasers.splice(i, 1);
+                enemies.splice(j, 1);
                 score++;
-                document.getElementById('score').innerHTML = "Score: " + score;
+                document.getElementById('score').innerText = 'Score: ' + score;
                 break;
             }
         }
     }
 
-    for (let i = 0; i < enemyLasers.length; i++) {
-        if (enemyLasers[i].x < player.x + player.width &&
-            enemyLasers[i].x + enemyLasers[i].width > player.x &&
-            enemyLasers[i].y < player.y + player.height &&
-            enemyLasers[i].y + enemyLasers[i].height > player.y) {
+    // Game Over
+    for (var i = enemyLasers.length - 1; i >= 0; i--) {
+        var laser = enemyLasers[i];
+        if (laser.x < player.x + player.width &&
+            laser.x + laser.width > player.x &&
+            laser.y < player.y + player.height &&
+            laser.y + laser.height > player.y) {
+            // Collision detected
             clearInterval(gameInterval);
             alert('Game Over!');
-            document.location.reload();
+            break;
         }
     }
-}
 
-function spawnEnemy() {
-    enemies.push({
-        x: Math.random() * (canvas.width - 50),
-        y: 0,
-        width: 50,
-        height: 50
-    });
+    // Game Over by Enemy Collision
+    for (var i = enemies.length - 1; i >= 0; i--) {
+        var enemy = enemies[i];
+        if (enemy.x < player.x + player.width &&
+            enemy.x + enemy.width > player.x &&
+            enemy.y < player.y + player.height &&
+            enemy.y + enemy.height > player.y) {
+            // Collision detected
+            clearInterval(gameInterval);
+            alert('Game Over!');
+            break;
+        }
+    }
+
+    // Remove enemies outside the screen
+    for (var i = enemies.length - 1; i >= 0; i--) {
+        var enemy = enemies[i];
+        if (enemy.y > canvas.height) {
+            enemies.splice(i, 1);
+        }
+    }
 }
 
